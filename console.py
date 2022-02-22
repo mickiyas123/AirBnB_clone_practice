4d8e0f612be00416b0d1429f54499e1924819f52
mickiy123 <mikikinfe345@gmail.com>
2022-02-22 16:09:59 +0300

    Add: add task 6
    create a console module for handling command line interaction

---
+++ console.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""console module that contains the
+   entry point of the command interpreter
+"""
+
+# Standard Library imports
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """ class for command interpreter implimentation """
+    prompt = '(hbnb) '
+
+    def emptyline(self):
+        """ Quit command to exit the program """
+        pass
+    
+    def do_quit(self, inputs):
+        """ a method to quit """
+        quit()
+
+    def do_EOF(self, inputs):
+        """ method for exit """
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
